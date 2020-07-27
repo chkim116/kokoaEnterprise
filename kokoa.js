ed2bf48127dedacf0a89a7a84a89e035e7abe26f
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-27 18:09:36 +0900

    폴더정리

--- kokoa.js
+++ kokoa.js
@@ -3,20 +3,18 @@ const firstText = document.querySelector(".item__text:first-child");
 
 //  텍스트 슬라이드
 function textShow() {
-        const currentText = document.querySelector(".text__visual");
-    if(currentText) {
-        currentText.classList.remove(TEXTVISUAL);
-        const nextText = currentText.nextElementSibling;
-        if (nextText) {
-            nextText.classList.add(TEXTVISUAL);
-            
-        } else {
-            firstText.classList.add(TEXTVISUAL);
-        }
+  const currentText = document.querySelector(".text__visual");
+  if (currentText) {
+    currentText.classList.remove(TEXTVISUAL);
+    const nextText = currentText.nextElementSibling;
+    if (nextText) {
+      nextText.classList.add(TEXTVISUAL);
     } else {
-        firstText.classList.add(TEXTVISUAL);
-
+      firstText.classList.add(TEXTVISUAL);
     }
+  } else {
+    firstText.classList.add(TEXTVISUAL);
+  }
 }
 
 textShow();
@@ -26,16 +24,14 @@ setInterval(textShow, 4000);
 const navbar = document.querySelector("#nav");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-
 function navbarFixing() {
-    if(navbarHeight < window.scrollY) {
-        navbar.classList.add('navbar__scroll');
-    }   
-    else {
-    navbar.classList.remove('navbar__scroll');
+  if (navbarHeight < window.scrollY) {
+    navbar.classList.add("navbar__scroll");
+  } else {
+    navbar.classList.remove("navbar__scroll");
+  }
 }
-}
-document.addEventListener('scroll', navbarFixing);
+document.addEventListener("scroll", navbarFixing);
 
 // 넷바 건들면 고정
 
@@ -44,50 +40,47 @@ const navbarMenu = document.querySelectorAll(".navbar_menu");
 const navbarSub = document.querySelector(".sub__showing");
 const wrapSub = document.querySelectorAll(".wrap__sub");
 const nav = document.querySelector("#nav");
-const header = document.querySelector(".header")
-
+const header = document.querySelector(".header");
 
 function showSub(e) {
-    const wrap = e.target.childNodes[3];
-    wrapSub.forEach((sub) => {
-        if (wrapSub) {
-            sub.classList.add('sub__showing');
-            nav.style.backgroundColor = "white";
-          
-        }
-    })
+  const wrap = e.target.childNodes[3];
+  wrapSub.forEach((sub) => {
+    if (wrapSub) {
+      sub.classList.add("sub__showing");
+      nav.style.backgroundColor = "white";
+    }
+  });
 }
 
 function downSub() {
-    wrapSub.forEach((sub) => {
-        if (wrapSub) {
-            sub.classList.remove('sub__showing');
-            nav.style.backgroundColor = "white";
-        }
-    })
+  wrapSub.forEach((sub) => {
+    if (wrapSub) {
+      sub.classList.remove("sub__showing");
+      nav.style.backgroundColor = "white";
+    }
+  });
 }
 function showBar() {
-    nav.style.backgroundColor = "white";
+  nav.style.backgroundColor = "white";
 }
 function downBar() {
-    nav.style.backgroundColor = "";
+  nav.style.backgroundColor = "";
 }
 
-for (i=0; i < navbarMenu.length; i++) {
-    navbarMenu[i].addEventListener("mouseover", showSub);
-    navbarMenu[i].addEventListener("mouseleave", downSub);
-    nav.addEventListener("mouseover", showBar);
-    nav.addEventListener("mouseleave", downBar);
+for (i = 0; i < navbarMenu.length; i++) {
+  navbarMenu[i].addEventListener("mouseover", showSub);
+  navbarMenu[i].addEventListener("mouseleave", downSub);
+  nav.addEventListener("mouseover", showBar);
+  nav.addEventListener("mouseleave", downBar);
 }
-
 
 // 푸터 관련사이트 버튼
 const relBtn = document.querySelector(".btn_relation");
 const relList = document.querySelector(".relation__list");
 const SHOWING = "showing";
 
-function showList () {
-    relList.classList.toggle(SHOWING);
+function showList() {
+  relList.classList.toggle(SHOWING);
 }
 
 relBtn.addEventListener("click", showList);
